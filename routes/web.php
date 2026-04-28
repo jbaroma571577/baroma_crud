176ad0d71d88df9be342b2bb6bfb9733771dce93
Jake <j.baroma.571577@umindanao.edu.ph>
2026-04-28 08:34:09 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,32 +6,31 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\RiceController;
 
-// Public routes
+
 Route::get('/', function () {
     return redirect('/rice');
 });
 
-// Authentication routes
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-// Protected routes
+
 Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    // Rice items
+   
     Route::resource('rice', RiceController::class);
 
-    // Orders
+ 
     Route::resource('orders', OrderController::class)
         ->only(['index', 'create', 'store', 'show']);
 
-    // Payments (FIXED)
+    
     Route::get('/payments', [PaymentController::class, 'index'])
         ->name('payments.index');
 
